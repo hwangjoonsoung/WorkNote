8dcbb64bd6d2a84bd6b9ec4526471a92654b4e3d
hwangjoonsoung <joonsoung93@naver.com>
2023-03-15 17:59:50 +0900

    Day 03 15

--- src/sqlDatetimeToLocalDateTime.java
+++ src/sqlDatetimeToLocalDateTime.java
@@ -1,15 +1,16 @@
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
+import java.util.UUID;
 
 public class sqlDatetimeToLocalDateTime {
 
     public static void main(String[] args) {
 
-        Date date = new Date(0);
-        LocalDateTime time = LocalDateTime.now();
-        System.out.println("date : "+date);
-        System.out.println(time.atZone(ZoneId.of("KOR")));
+
+        UUID uuid = UUID.randomUUID();
+        System.out.println(uuid);
 
     }
+
 }
