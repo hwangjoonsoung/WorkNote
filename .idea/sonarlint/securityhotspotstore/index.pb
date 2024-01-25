e7ffd396d467d94849b5935ae88794857cf0630f
hwangjoonsoung <joonsoung93@cricmail.net>
2024-01-25 17:22:07 +0900

    Day 24 01 25

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,3 @@
+
+B
+src/test/test.java,c\a\ca8162c62f83b7a9284e2dccc9dd7b6517018fed
--- src/test/test.java
+++ src/test/test.java
@@ -1,21 +1,13 @@
 package test;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 public class test {
 
     public static void main(String[] args) throws ParseException {
 
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-        System.out.println(formatter.parse(""));
-
-        test test = new test();
-        test.of();
+        String t = "South Georgia and the South Sandwich Islands";
+        System.out.println(t.length());
     }
 
-    public test of() {
-
-        return this;
-    }
 }
